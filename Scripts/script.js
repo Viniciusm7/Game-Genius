a1c6fd89e34d4509797a1a13ab9e5fec33a145ff
viniciusm7 <melgaco97@outlook.com>
2021-11-05 12:40:34 -0300

    adicionado funçao para acender em ordens aleatorias

--- Scripts/script.js
+++ Scripts/script.js
@@ -13,3 +13,27 @@ const blue = document.querySelector('.blue');
 const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
+//função para o clink dos butoes 
+let shuffledOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+    // 
+    for(let i in order) {
+        let elementColor = creatElement(order[i]);
+        lightColor(elementColor, Number(i + 1));
+    }
+
+}
+// acender os boteos nas ordens aleatorias
+let lightColor = (element, number) => {
+    time = time * 500;
+    setTimeout(() => {
+        element.classList.add('select');
+    }, tempo - 250 );
+    // remove o tempo criado em cima
+    setTimeout(() => {
+        element.classList.remove('select')
+    })
+
+}
