49f25b95e6b42f18526651432fc56f90a7844f69
viniciusm7 <melgaco97@outlook.com>
2021-11-05 12:07:27 -0300

    commit inicial

---
+++ Scripts/script.js
@@ -0,0 +1,15 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+
+//0 igual ao verde
+//1 igual ao vermelhor
+//2 igula ao amarelo 
+//3 ogual ao azul
+
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
